d11c20e23f401777e07df109ab3ad15c35067e7e
vattgern <sashawot060@gmail.com>
2023-03-01 17:20:14 +0400

    Товары готово редактирование картинку плохо работает

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductEditRequest;
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
-    //
+    public function all(): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'message' => 'Все товары',
+            'content' => Product::all()
+        ], 200);
+    }
+    public function index($id): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'message' => 'Вывод товара',
+            'content' => Product::find($id)
+        ], 200);
+    }
+    public function store(ProductRequest $request): \Illuminate\Http\JsonResponse
+    {
+        $file = $request->file('image');
+        $path = Storage::disk('public')->put('products', $file);
+
+        $product = Product::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'price' => $request->input('price'),
+            'structure' => $request->input('structure'),
+            'image' => $path
+        ]);
+
+        return response()->json([
+            'message' => 'Товар создан',
+            'content' => $product
+        ], 200);
+    }
+    public function update(ProductEditRequest $request, $id): \Illuminate\Http\JsonResponse
+    {
+        $product = Product::find($id);
+        $file = $request->file('image');
+        $path = '';
+
+        if(!$product){
+            return response()->json([
+                'message' => 'Товара не существует'
+            ]);
+        }
+        if($file){
+            Storage::disk('public')->delete($product->image);
+            $path = Storage::disk('public')->put('products', $file);
+            $product->image = $path;
+            $product->save();
+        }
+        $product->update($request->all());
+
+        return response()->json([
+            'content' => [
+                'content' => $product,
+                'message' => 'Данные обновлены'
+            ]
+        ], 200);
+    }
+    public function destroy($id): \Illuminate\Http\JsonResponse
+    {
+        $product = Product::find($id);
+        if(!$product){
+            return response()->json([
+                'message' => 'Товар уже удален'
+            ]);
+        }
+        Storage::disk('public')->delete($product->image);
+        $product->delete();
+
+        return response()->json([
+            'message' => 'Товар удален'
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -23,6 +25,17 @@ Route::middleware('user')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::get('/logout', [AuthController::class, 'logout']);
 });
+
+// Товары
+Route::get('/products', [ProductController::class, 'all']);
+Route::get('/product/{id}', [ProductController::class, 'index']);
+Route::post('/product', [ProductController::class, 'store']);
+Route::patch('/product/{id}', [ProductController::class, 'update']);
+Route::delete('/product/{id}', [ProductController::class, 'destroy']);
+
+// Оформление заказа
+Route::post('/cart/{id}', [CartController::class, 'store']);
+
 
 // Пользователи
 //Route::get('/users', [UserController::class, 'all']);
